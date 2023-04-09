a89015bc2bf3537ecf15f6258f0f30eb185f1b7a
gentasuz <4genta89@gmail.com>
2023-04-09 19:08:27 +0900

    8-6

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/create', [Postcontroller::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
